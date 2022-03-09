eb75d1db73a40ddc0378c11ab77c1092f1e3ca46
Matilde Aarenstrup Christoffersen <mati1398@edu.easj.dk>
2022-03-09 08:38:15 +0100

    Add project files.

---
+++ CarLibTests/CarTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLib.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car car1;
+        private Car car2;
+        [TestInitialize]
+        public void init()
+        {
+            car1 = new Car("Volvo", 125000, "AC 955", 1);
+            car2 = new Car("Fiat", 89000, "KY 876", 2);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            Assert.AreEqual(1, car1.Id);
+            Assert.AreEqual(2, car2.Id);
+        }
+
+        [TestMethod()]
+        public void CarModelTest()
+        {
+            Assert.AreEqual("Volvo" , car1.Model);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => car1.Model = "Kia");
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.AreEqual(125000, car1.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => car1.Price = -1);
+        }
+
+        [TestMethod()]
+        public void LicensePlateTest()
+        {
+            Assert.AreEqual("AC 955", car1.LicensePlate);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => car1.LicensePlate = "12345678");
+        }
+    }
+}
---
+++ MathLibrary/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CarLib
+{
+    public class Car
+    {
+        
+        private string _model;
+        private double _price;
+        private string _licensePlate;
+
+        public int Id { get; set; }
+
+        public Car(string model, double price, string licensePlate, int id)
+        {
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+            Id = id;
+
+        }
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value.Length < 4) { throw new ArgumentOutOfRangeException();}
+
+                _model = value;
+            }
+
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) { throw new ArgumentOutOfRangeException();}
+
+                _price = value;
+            }
+             
+        }
+
+        public string LicensePlate
+        {
+            get => _licensePlate;
+            set
+            {
+                if (value.Length < 2 || value.Length > 7) {throw new ArgumentOutOfRangeException();}
+
+                _licensePlate = value;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+    }
